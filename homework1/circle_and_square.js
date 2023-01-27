8194ee1173b8def30a8fc1a9e8c596610872e8e5
jac2001 <113481735+jac2001@users.noreply.github.com>
2023-01-27 14:43:25 -0600

    Add files via upload

---
+++ homework1/circle_and_square.js
@@ -0,0 +1,12 @@
+
+// index.html
+function setup() {
+    createCanvas(195, 100);
+  }
+  
+  function draw() {
+    background('rgb(0,255,0)');
+    ellipse(50,50,80,80);
+    square(100,10,80)
+    
+  }
---
+++ homework1/venn_diagram.js
@@ -0,0 +1,23 @@
+//index2.html
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+ 
+  background(250,250,250);
+  colorMode(RGB, 100);
+ 
+  noStroke()
+  fill(100,0,0,30);
+  ellipse(250,100,130,130);
+  noStroke()
+  fill(0,100,0,30);
+  ellipse(300,170,130,130);
+  noStroke()
+    fill(0,0,100,30);
+  ellipse(210,170,130,130);
+  
+
+  
+}
